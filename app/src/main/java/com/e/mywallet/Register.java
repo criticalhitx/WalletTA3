74657ecf2bcf1e7078311faed82a656873bd6491
criticalhitx <clink200032@gmail.com>
2019-10-21 03:10:49 +0700

    MALAM2 hari sabtu

--- app/src/main/java/com/e/mywallet/Register.java
+++ app/src/main/java/com/e/mywallet/Register.java
@@ -1,18 +1,48 @@
 package com.e.mywallet;
-
+import com.physicaloid.lib.Physicaloid;
+import com.physicaloid.lib.usb.driver.uart.ReadLisener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.physicaloid.lib.Physicaloid;
+import com.physicaloid.lib.usb.driver.uart.ReadLisener;
+//import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
     EditText etUsername, etPassword, etEmail,etFirstName,etLastName;
-    TextView tv1,tv2,tv3,tv4,tv5;
+    TextView tv1,tv2,tv3,tv4,tv5,tvRegResponse;
+    Spinner spBauds;
+    CheckBox cbAutoscrolls;
     String firstname,lastname,user_name,user_pass,email;
-    Button btnReg;
+    String pkNew; //pk untuk didaftarkan
+    Button btnReg,btOpen;
+    Physicaloid mPhysicaloid; // initialising library
+    boolean canexit=false;
+    ImageView imagebackPin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +53,10 @@ public class Register extends AppCompatActivity {
         etEmail=(EditText)findViewById(R.id.registerEmail);
         etFirstName=(EditText)findViewById(R.id.registerFirstName);
         etLastName = (EditText)findViewById(R.id.registerLastName);
+
         btnReg = (Button)findViewById(R.id.registerBtnReg);
+        imagebackPin=(ImageView) findViewById(R.id.Register_Image);
+
 
         tv1=(TextView)findViewById(R.id.registerText1);
         tv2=(TextView)findViewById(R.id.registerText2);
@@ -31,34 +64,125 @@ public class Register extends AppCompatActivity {
         tv4=(TextView)findViewById(R.id.registerText4);
         tv5=(TextView)findViewById(R.id.registerText5);
 
+        btOpen =(Button)findViewById(R.id.Register_Open);
+        spBauds = (Spinner) findViewById(R.id.Register_spinnerz);
+        cbAutoscrolls = (CheckBox)findViewById(R.id.Register_autoscrollz);
+        tvRegResponse = (TextView)findViewById(R.id.registerResponse);
+        setEnabledUi(false);
 
-       /* btnReg.setOnClickListener(new View.OnClickListener()
+        mPhysicaloid = new Physicaloid(this);
+        Toast.makeText(this, "by Pressing Connect Button, it will erase the data inside wallet", Toast.LENGTH_LONG).show();
+
+       btOpen.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                tv1.setText(etUsername.getText().toString());
-                tv1.setVisibility(View.VISIBLE);
-                tv2.setText(etPassword.getText().toString());
-                tv2.setVisibility(View.VISIBLE);
-                tv3.setText(etEmail.getText().toString());
-                tv3.setVisibility(View.VISIBLE);
-                tv4.setText(etFirstName.getText().toString());
-                tv4.setVisibility(View.VISIBLE);
-                tv5.setText(etLastName.getText().toString());
-                tv5.setVisibility(View.VISIBLE);
+                String baudtext = spBauds.getSelectedItem().toString();
+                switch (baudtext) {
+                    case "300 baud":
+                        mPhysicaloid.setBaudrate(300);
+                        break;
+                    case "1200 baud":
+                        mPhysicaloid.setBaudrate(1200);
+                        break;
+                    case "2400 baud":
+                        mPhysicaloid.setBaudrate(2400);
+                        break;
+                    case "4800 baud":
+                        mPhysicaloid.setBaudrate(4800);
+                        break;
+                    case "9600 baud":
+                        mPhysicaloid.setBaudrate(9600);
+                        break;
+                    case "19200 baud":
+                        mPhysicaloid.setBaudrate(19200);
+                        break;
+                    case "38400 baud":
+                        mPhysicaloid.setBaudrate(38400);
+                        break;
+                    case "576600 baud":
+                        mPhysicaloid.setBaudrate(576600);
+                        break;
+                    case "744880 baud":
+                        mPhysicaloid.setBaudrate(744880);
+                        break;
+                    case "115200 baud":
+                        mPhysicaloid.setBaudrate(115200);
+                        break;
+                    case "230400 baud":
+                        mPhysicaloid.setBaudrate(230400);
+                        break;
+                    case "250000 baud":
+                        mPhysicaloid.setBaudrate(250000);
+                        break;
+                    default:
+                        mPhysicaloid.setBaudrate(9600);
+                }
 
-                firstname=etFirstName.getText().toString();
-                user_name=etUsername.getText().toString();
-                user_pass=etPassword.getText().toString();
-                String method ="register";
-                BackgroundTask backgroundTask =new BackgroundTask(this);
-                backgroundTask.execute(method,firstname,user_name,user_pass);
+                if(mPhysicaloid.open()) {
+                    setEnabledUi(true);
+                    String kirim = "3"; //Mengirim case 3 ke while loop
+                    if(kirim.length()>0) {
+                        byte[] buf = kirim.getBytes();
+                        mPhysicaloid.write(buf, buf.length);
+                    }
 
 
+                   /* if(mPhysicaloid.open()) { // Pesan ini diberikan wallet
+
+                    }*/
+                    byte[] bufs = new byte[256];
+                    mPhysicaloid.read(bufs, bufs.length);
+                    String str = new String(bufs);
+                    tvRegResponse.setText(null);
+                    tvRegResponse.setText(str); // ini pesannya contoh " privatekey yang akan didaftarkan" .
+                    pkNew=tvRegResponse.getText().toString();
+                    // mPhysicaloid.close();
+
+                    if(cbAutoscrolls.isChecked())
+                    {
+                        tvRegResponse.setMovementMethod(new ScrollingMovementMethod());
+                    }
+                    mPhysicaloid.addReadListener(new ReadLisener() {
+                        @Override
+                        public void onRead(int size) {
+                            byte[] buf = new byte[size];
+                            mPhysicaloid.read(buf, size);
+                            tvAppend(tvRegResponse, Html.fromHtml( new String(buf) ));
+                        }
+                    });
+
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(),"Not Connect",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+
+        imagebackPin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String kirim = "moemoe";
+                if(kirim.length()>0) {
+                    byte[] buf = kirim.getBytes();
+                    mPhysicaloid.write(buf, buf.length);}
+
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        canexit=true;
+                        mPhysicaloid.close();
+                        onBackPressed();
+                    }
+                }, 1500);
 
             }
-        });*/
+        });
     }
 
 
@@ -77,5 +201,54 @@ public class Register extends AppCompatActivity {
         backgroundTask.execute(method,firstname,user_name,user_pass,email,lastname);
         finish();
     }
+
+
+
+    public void onBackPressed(){
+        if (canexit) {
+            super.onBackPressed();
+
+        }
+    }
+
+    private void setEnabledUi(boolean on) {
+        if(on) {
+            btOpen.setEnabled(false);
+            btnReg.setEnabled(true);
+            spBauds.setEnabled(false);
+            cbAutoscrolls.setEnabled(false);
+            tvRegResponse.setEnabled(true);
+            etLastName.setEnabled(true);
+            etFirstName.setEnabled(true);
+            etPassword.setEnabled(true);
+            etEmail.setEnabled(true);
+            etUsername.setEnabled(true);
+        } else {
+            btOpen.setEnabled(true);
+            btnReg.setEnabled(false);
+            spBauds.setEnabled(true);
+            cbAutoscrolls.setEnabled(true);
+            tvRegResponse.setEnabled(false);
+            etLastName.setEnabled(false);
+            etFirstName.setEnabled(false);
+            etPassword.setEnabled(false);
+            etEmail.setEnabled(false);
+            etUsername.setEnabled(false);
+        }
+    }
+
+
+    Handler mHandler = new Handler();
+    private void tvAppend(TextView tv, CharSequence text) {
+        final TextView ftv = tv;
+        final CharSequence ftext = text;
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                ftv.append(ftext);
+            }
+        });
+    }
+
 
 }
